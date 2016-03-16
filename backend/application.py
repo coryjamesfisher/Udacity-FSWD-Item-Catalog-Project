33c4a9ff4cedb81ade54d0dcaac0479ca5d64b6d
Cory Fisher <coryjamesfisher@gmail.com>
2016-03-16 06:28:05 -0400

    Fix flask to run on port 8080 and be exposed from Vagrant box

--- backend/application.py
+++ backend/application.py
@@ -14,4 +14,4 @@ app.register_blueprint(categories.categories_rest)
 app.register_blueprint(items.items_rest)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=8080)
